d76697020f1a33e5d349644ce00125bff8a7031a
Hidekidev1 <hideki.oy@mail.com>
2025-07-08 11:47:44 -0300

    format

---
+++ utils-helpers.py
@@ -0,0 +1,37 @@
+def validar_cpf(cpf):
+    # Remover caracteres não numéricos
+    cpf = ''.join(filter(str.isdigit, cpf))
+    
+    # Verificar se tem 11 dígitos
+    if len(cpf) != 11:
+        return False
+    
+    # Verificar se todos os dígitos são iguais
+    if cpf == cpf[0] * 11:
+        return False
+    
+    # Validar primeiro dígito verificador
+    soma = 0
+    for i in range(9):
+        soma += int(cpf[i]) * (10 - i)
+    resto = soma % 11
+    digito1 = 0 if resto < 2 else 11 - resto
+    
+    if int(cpf[9]) != digito1:
+        return False
+    
+    # Validar segundo dígito verificador
+    soma = 0
+    for i in range(10):
+        soma += int(cpf[i]) * (11 - i)
+    resto = soma % 11
+    digito2 = 0 if resto < 2 else 11 - resto
+    
+    if int(cpf[10]) != digito2:
+        return False
+    
+    return True
+
+def formatar_cpf(cpf):
+    cpf = ''.join(filter(str.isdigit, cpf))
+    return f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
